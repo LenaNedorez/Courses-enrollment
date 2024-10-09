f80d1734e0791041e6064922900724fbe2e5b6d2
Elena Nedorezova <nedorezova.emp@gmail.com>
2024-10-09 16:54:05 +0300

    added main, changed pom.xml

---
+++ src/main/java/ru/nedorezova/CoursesEnrollmentApplication.java
@@ -0,0 +1,12 @@
+package ru.nedorezova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesEnrollmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesEnrollmentApplication.class, args);
+    }
+}
