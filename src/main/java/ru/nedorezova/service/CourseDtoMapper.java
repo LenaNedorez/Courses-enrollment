6832d901d173308887a991f3f756a3d284d52ed0
Elena Nedorezova <nedorezova.emp@gmail.com>
2024-10-10 18:29:40 +0300

    added CourseDtoMapper

---
+++ src/main/java/ru/nedorezova/service/CourseDtoMapper.java
@@ -0,0 +1,18 @@
+package ru.nedorezova.service;
+
+import ru.nedorezova.dto.CourseDto;
+import ru.nedorezova.entity.Course;
+
+public class CourseDtoMapper {
+
+    public static CourseDto convertToDto(Course course) {
+        CourseDto courseDto = new CourseDto();
+        courseDto.setId(course.getId());
+        courseDto.setName(course.getName());
+        courseDto.setCapacity(course.getCapacity());
+        courseDto.setCurrentEnrollment(course.getCurrentEnrollment());
+        courseDto.setStartDate(course.getStartDate());
+        courseDto.setEndDate(course.getEndDate());
+        return courseDto;
+    }
+}
