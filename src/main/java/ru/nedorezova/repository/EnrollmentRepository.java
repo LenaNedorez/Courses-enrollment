c237f2ad560d5c2dbd05a57ebfeecebe63386a69
Elena Nedorezova <nedorezova.emp@gmail.com>
2024-10-12 01:02:07 +0300

    changed repositories

--- src/main/java/ru/nedorezova/repository/EnrollmentRepository.java
+++ src/main/java/ru/nedorezova/repository/EnrollmentRepository.java
@@ -1,11 +1,14 @@
 package ru.nedorezova.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.nedorezova.entity.Enrollment;
 
 import java.util.List;
 
+@Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
     List<Enrollment> findByStudentId(Long id);
+
 }
--- src/main/java/ru/nedorezova/repository/StudentRepository.java
+++ src/main/java/ru/nedorezova/repository/StudentRepository.java
@@ -1,7 +1,10 @@
 package ru.nedorezova.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.nedorezova.entity.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    
 }
