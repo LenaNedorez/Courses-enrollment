f44dadab0cac96a20fcc4530d609f5b24aaac1f4
Elena Nedorezova <nedorezova.emp@gmail.com>
2024-10-18 22:41:11 +0300

    added @Column to Enrollment entity

--- src/main/java/ru/nedorezova/entity/Enrollment.java
+++ src/main/java/ru/nedorezova/entity/Enrollment.java
@@ -18,7 +18,9 @@ public class Enrollment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "student_id")
     private Long studentId;
+    @Column(name = "course_id")
     private Long courseId;
 
 }
